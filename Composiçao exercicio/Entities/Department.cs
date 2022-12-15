948a49c760032e07af51c369a5c467a72cc90532
Danilo Ramos <danilodramos@yahoo.com.br>
2022-12-15 18:03:09 -0300

    Exercicio

---
+++ Composiçao exercicio/Entities/Department.cs
@@ -0,0 +1,15 @@
+﻿namespace Composiçao_exercicio.Entities
+{
+    class Department
+    {
+        public string Name { get; set; }
+        public Department()
+        {
+
+        }
+        public Department(string name)
+        {
+            Name = name;
+        }
+    }
+}
---
+++ Composiçao exercicio/Entities/Enums/WorkerLevel.cs
@@ -0,0 +1,11 @@
+﻿
+
+namespace Composiçao_exercicio.Entities.Enums
+{
+    enum WorkerLevel : int
+    {
+        Junior = 0,
+        MidLevel = 1,
+        Senior = 2
+    }
+}
